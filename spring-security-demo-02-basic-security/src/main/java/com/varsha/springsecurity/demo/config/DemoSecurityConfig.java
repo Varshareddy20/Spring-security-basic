eadc7fce211a6124d1cc08fee1559491387a6c50
2147006 <2147006@LTIN430051.cts.com>
2023-07-24 18:15:34 +0530

    My seventh commit

---
+++ spring-security-demo-02-basic-security/src/main/java/com/varsha/springsecurity/demo/config/DemoSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.varsha.springsecurity.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		 
+		 //add our users for in memory authentication
+		
+		UserBuilder users = User.withDefaultPasswordEncoder();
+
+		
+		auth.inMemoryAuthentication()
+			.withUser(users.username("varsha").password("me123").roles("Employee"))
+			.withUser(users.username("Jay").password("me123").roles("Associate"))
+			.withUser(users.username("Sandeep").password("me123").roles("Admin"));
+
+	}
+	
+	
+}
